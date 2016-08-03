96da342b48805e6f17067c2c9e5f97e8a621c86a
Jeff <jeffrey.pierson@gmail.com>
2016-08-03 17:54:21 -0400

    Added code to actually send message to sharding gateway

--- x-akka-sharding/Program.cs
+++ x-akka-sharding/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Akka.Actor;
+using Akka.Cluster.Tools.Singleton;
+using Akka.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +26,9 @@ namespace x_akka_sharding
 
             foreach (var i in Enumerable.Range(0, 100000))
             {
-                new DomainEvent(i, randomNumberGenerator.Next(1, 10));
+                var e = new DomainEvent(i, randomNumberGenerator.Next(1, 10));
+                gateway.Tell(e);
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
             }
 
             system.WhenTerminated.Wait();
